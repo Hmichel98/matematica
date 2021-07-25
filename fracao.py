1f4e698fda3e91a078f2f019de83a76c06319ba9
hmich_l <69561302+Hmichel98@users.noreply.github.com>
2021-07-25 17:13:50 -0300

    atualizando fracao.py

--- fracao.py
+++ fracao.py
@@ -1,4 +1,4 @@
-from simplificar import *
+from simplificar import somar_fracoes, subtrair_fracoes
 
 
 class Fracao:
@@ -26,6 +26,14 @@ class Fracao:
         return Fracao(*subtrair_fracoes(self.numerador, self.denominador, fracao2.numerador, fracao2.denominador))
 
 
-print(Fracao(2, 3) + Fracao(2, 3))
-print(sum([Fracao(2, 3), Fracao(2, 3), Fracao(4, 5)]))
-print(Fracao(3, 2) - Fracao(4, 5)) 
+if __name__ == "__main__":
+    print(Fracao(2, 3) + Fracao(2, 3))
+    #    4/ 3
+    print(sum([Fracao(2, 3), Fracao(2, 3), Fracao(4, 5)]))
+    #    32/ 15
+    print(Fracao(3, 2) - Fracao(4, 5))
+    #    7/ 10
+
+
+
+
